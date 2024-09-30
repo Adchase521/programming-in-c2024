e56937dc3666c91be4e5a5adf94382adc1431406
Adchase521 <adrianmwangi619@gmail.com>
2024-09-30 16:25:13 +0300

    Create Condition.C

---
+++ Condition.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{ 
+     int num1 = num1;
+     int num2 = num2;
+     if(num1 < num2)
+     //test-condition 
+           {
+           printf("num1 is smaller than num 2");
+              }
+              return 0;
+}              
